8e88eb68f11c59900aef6eebc66430458e2a2b49
Bobrock <barret.obrock@gmail.com>
2021-06-13 10:40:05 -0500

    Add documentation to client scripts

--- client_scripts/client_push_changes.sh
+++ client_scripts/client_push_changes.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+#/      --push_changes.sh--
+#/  Pushes a commit to master while also incrementing the version based on
+#/      the "level" of changes that have taken place and tagging that onto the commit.
+#/
+#/  Usage: push_changes.sh [options]
+#/
+#/  Options
+#/      -v|--version                        Prints script name & version.
+#/      -l|--level (patch|minor|major)      Sets level of update, to determine version bump (default: patch)
+#/
+
 # Calls the push_changes file at whatever path the py-package-manager repo is in
 PPM_PATH=../py-package-manager/push_changes.sh
 
